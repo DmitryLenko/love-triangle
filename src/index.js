098fe2120fda193bdd3c433de07478c2c0280cb6
DmitryLenko <dias1381254@gmail.com>
2018-09-11 23:05:37 +0300

    all tests working

--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var Spichonee1 = 0;
+  var Spichonee2 = 0;
+  var Spichonee3 = 0;
+  var counter = 0;
+
+  for(let i = preferences.length - 1, loveTriangle = preferences.length; i>=0; i-- , loveTriangle--) {
+    Spichonee1 = preferences[i];
+    Spichonee2 = preferences[Spichonee1-1];
+    Spichonee3 = preferences[Spichonee2-1];
+    if (loveTriangle !== Spichonee2 && loveTriangle === Spichonee3) {
+      counter++;
+      preferences.length--;
+    } else {
+      preferences.length--;
+    }  
+  }
+  return counter;
 };
